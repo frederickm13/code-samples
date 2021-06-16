0fed3d0c4b439fe02489441d9c23c608e54af0c7
Erick McCollum <25780431+frederickm13@users.noreply.github.com>
2021-06-16 08:14:01 -0500

    Add HttpClient-SendAsync folder

---
+++ dotnet/HttpClient-SendAsync/HttpClientPostAsync.cs
@@ -0,0 +1,18 @@
+// Code snippet demonstrating how to send an HTTP POST request 
+// using the HttpClient.PostAsync() method.
+// Associated article: https://www.erickmccollum.com/2021/06/16/httpclient-sendasync.html.
+
+// ...
+
+    // Create Uri and POST body content.
+    Uri requestUri = new Uri("https://example.com/api/");
+    StringContent content = new StringContent("{ \"firstName\": \"John\", \"lastName\": \"Doe\"}");
+
+    // Create HttpClient instance and set OAuth header.
+    using HttpClient client = new HttpClient();
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "{OAuth token}");
+
+    // Send HTTP POST request.
+    using HttpResponseMessage response = await client.PostAsync(requestUri, content);
+
+// ...
---
+++ dotnet/HttpClient-SendAsync/HttpClientSendAsync.cs
@@ -0,0 +1,22 @@
+// Code snippet demonstrating how to send an HTTP POST request 
+// using the HttpClient.SendAsync() method.
+// Associated article: https://www.erickmccollum.com/2021/06/16/httpclient-sendasync.html.
+
+// ...
+
+    // Create Uri and POST body content.
+    Uri requestUri = new Uri("https://example.com/api/");
+    StringContent content = new StringContent("{ \"firstName\": \"John\", \"lastName\": \"Doe\"}");
+
+    // Create HttpClient instance.
+    using HttpClient client = new HttpClient();
+
+    // Create HttpRequestMessage and set method, OAuth header, and content.
+    using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUri);
+    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "{OAuth token}");
+    request.Content = content;
+
+    // Send HTTP POST request.
+    using HttpResponseMessage response = await client.SendAsync(request);
+
+// ...
